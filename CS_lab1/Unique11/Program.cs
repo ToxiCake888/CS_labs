21ea097166a66600cfedb1571eb5f6eeb97c3531
ToxiCake888 <keksik888keksik@gmail.com>
2023-09-24 18:16:09 +0300

    cumitstagedchanges

--- CS_lab1/Unique11/Program.cs
+++ CS_lab1/Unique11/Program.cs
@@ -1,19 +1,13 @@
-﻿namespace MathClassCS
+﻿class SUS
 {
-    class sus
+    static void Main()
     {
-        static void Main()
-        {
-
-            double x = 6.251, y = 0.827, z = 25.001, b_example = 0.7121 , e = 2.71828182846;
-            string b , b_old;
-            b_old = Convert.ToString((Math.Pow(y, Math.Pow(Math.Abs(x), 1.0 / 3))) + (Math.Pow(Math.Cos(y * (Math.Abs(x-y)-(1.0+(Math.Pow(Math.Sin(z),2))/Math.Pow(x+y, 0.5)))/ (Math.Pow(Math.Abs(x-y)+x*0.5, 3))),3)));
-            b = Convert.ToString(Math.Pow( y , Math.Pow( Math.Abs(x) , 1.0/3 ) ) + Math.Pow( Math.Cos( y * (Math.Abs(x-y) - (1.0 +(Math.Pow(Math.Sin(z) , 2))/(Math.Pow( (x+y) , 0.5)))) / ( Math.Pow( e , Math.Abs( x-y ) ) + (x / 2.0)) ) , 3 )                                                                    );
-            System.Console.WriteLine(b_old);
-            System.Console.WriteLine(b); //обрезать еще надо
-            System.Console.WriteLine(b_example);
-        }
+        string b_string;
+        double x = 6.251 , y = 0.827 , z = 25.001, b_example = 0.7121 , b , e = 2.71828182846;
+        b = Math.Pow(y , Math.Pow(Math.Abs(x), 1.0/3)) + y * (Math.Abs(x-y)-(1.0 + Math.Pow(Math.Sin(z),2)))/(Math.Pow(e,Math.Abs(x-y)) + x/2);
+        Console.WriteLine(b_example);
+        b_string = (Convert.ToString(b)).Substring(0, 7);
+        Console.WriteLine(b_string);
     }
 }
 
-            //(Math.Pow(Math.Sin(z) , 2))/(Math.Pow( (x+y) , 0.5))
---
+++ CS_lab1/Unique_3/Program.cs
@@ -0,0 +1,14 @@
+﻿class Main_program
+{
+    static void Main()
+    {
+        string v_string;
+        double x = 3.74 * Math.Pow(10, -2), y = -0.825, z = 0.16 * Math.Pow(10, 2), v_example = 1.0553, v;
+        v = (1.0 + Math.Pow(Math.Sin(x + y), 2)) * Math.Pow(x, Math.Abs(y)) / (Math.Abs( x - 2*y/(1.0 + Math.Pow(x , 2) * Math.Pow(y , 2)))) + Math.Pow(Math.Cos(Math.Atan(1.0 / z)), 2);
+        Console.Write("Пример: ");
+        Console.WriteLine(v_example);
+        Console.Write("Рассчетное значение: ");
+        v_string = (Convert.ToString(v)).Substring(0, 6);
+        Console.WriteLine(v_string);
+    }
+}
